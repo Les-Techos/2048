540ae197c41e69e4732258b1d4aef2a92b095515
Tim <tim.ksk@live.fr>
2022-05-04 17:11:24 +0200

    Use Maps

---
+++ src/modele/Coord.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class Coord {
+    int x,y;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/modele/Jeu.java
+++ src/modele/Jeu.java
@@ -1,45 +1,50 @@
 package modele;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Random;
 
 public class Jeu extends Observable {
 
-    private Case[][] tabCases;
+    int size;
+
+    private HashMap<Case, Coord> mp_case_coord = new HashMap<Case, Coord>();
+    private Map<Coord, Case> mp_coord_case = new HashMap<Coord, Case>();
+
     private static Random rnd = new Random(4);
 
     public Jeu(int size) {
-        tabCases = new Case[size][size];
+        this.size = size;
         rnd();
     }
 
     public int getSize() {
-        return tabCases.length;
+        return size;
     }
 
     public Case getCase(int i, int j) {
-        return tabCases[i][j];
+        return mp_coord_case.get(new Coord(i, j));
     }
-
 
     public void rnd() {
         new Thread() { // permet de libérer le processus graphique ou de la console
             public void run() {
                 int r;
 
-                for (int i = 0; i < tabCases.length; i++) {
-                    for (int j = 0; j < tabCases.length; j++) {
+                for (int i = 0; i < size; i++) {
+                    for (int j = 0; j < size; j++) {
                         r = rnd.nextInt(3);
 
                         switch (r) {
                             case 0:
-                                tabCases[i][j] = null;
+                                // tabCases[i][j] = null;
                                 break;
                             case 1:
-                                tabCases[i][j] = new Case(2);
+                                mp_coord_case.put(new Coord(i, j), new Case(2));
                                 break;
                             case 2:
-                                tabCases[i][j] = new Case(4);
+                                mp_coord_case.put(new Coord(i, j), new Case(4));
                                 break;
                         }
                     }
@@ -48,11 +53,16 @@ public class Jeu extends Observable {
 
         }.start();
 
-
         setChanged();
         notifyObservers();
 
-
     }
 
+    public void move(Direction d) {
+        switch (d) {
+            case gauche:
+
+                break;
+        }
+    }
 }
