20c48071a275070dae0ca2e19893c63483d0ecd5
Tim <tim.ksk@live.fr>
2022-06-04 14:26:42 +0200

    Clean Main(s).java

--- src/Main_test.java
+++ src/Main_test.java
@@ -12,84 +12,32 @@ import modele.Direction.Direction2D;
 import modele.Grille.Grille;
 import modele.Grille.Grille2D;
 import util.Serializer;
+import util.IA.IA_Action;
+import util.IA.MC_IA;
+import util.IA.IAReady.Grille2D_IA;
 
 public class Main_test {
     public static void main(String args[]) throws InterruptedException {
-        run();
-        // main2();
-    }
-
-    public static void main2() throws InterruptedException {
-        Grille2D g = new Grille2D(4); // Initialisation de la grille
-        for (int i = 0; i < 4; i++) // Insertion des cases
-            g.insertRandomCase();
-
-        System.out.println(g);
-        System.out.println(getBestAction(g));
-    }
-
-    public static void run() {
         long start = System.currentTimeMillis();
 
-        Grille2D g = new Grille2D(4); // Initialisation de la grille
+        MC_IA ia = new MC_IA();
+
+        Grille2D_IA g = new Grille2D_IA(4); // Initialisation de la grille
         for (int i = 0; i < 4; i++) // Insertion des cases
             g.insertRandomCase();
 
         int step = 0;
 
-        while (!(g.iswrecked())) { // Tant que notre noeud courant n'est pas terminé
+        while (!g.isFinished()) { // Tant que notre noeud courant n'est pas terminé
 
-            Direction2D max_dir = getBestAction(g);
-            step(g, max_dir);
+            IA_Action act = (IA_Action)ia.getBestAction(g);
+            g.step(act);
 
-            System.out.println("Etape n° " + step++ + " / " + max_dir);
+            System.out.println("Etape n° " + step++ + " / " + act.getAction());
             System.out.println(g);
         }
         System.out.println((System.currentTimeMillis() - start)/1000 + " s");
     }
 
-    public static Direction2D getBestAction(Grille2D base_g) {
-        Direction2D max_dir = null;
-        double max_score = -1; // On sauvegarde le maximum des moyennes des maximums
-        for (Direction2D dir : base_g.getAvailableDirections()) { // Pour chaque direction
-            Grille2D g = (Grille2D) base_g.clone();
-            step(g, dir);
-
-            double score = getScore(g, 70);
-
-            // System.out.println("Direction de départ : " + dir + " / Le score atteint est
-            // : " + max);
-
-            if (score > max_score) {
-                max_score = score;
-                max_dir = dir;
-            }
-        }
-        return max_dir;
-    }
-
-    public static double getScore(Grille2D base_grid, int nb_tries) {
-        Random r = new Random();
-        double score = 0;
-
-        for (int i = 0; i < nb_tries; i++) {
-            Grille2D g = (Grille2D) base_grid.clone();
-            int nb_steps = 0;
-            while (!(g.iswrecked())) {
-                ArrayList<Direction2D> poss = g.getAvailableDirections();
-                step(g, poss.get(Math.abs(r.nextInt()) % poss.size()));
-                if(g.iswinning()){
-                    nb_steps += nb_tries;
-                }
-                nb_steps++;
-            }
-            score += nb_steps;
-        }
-
-        return score/((double)nb_tries);
-    }
-
-    public static void step(Grille2D g, Direction2D d) {
-        g.move(d);
-    }
+  
 }
